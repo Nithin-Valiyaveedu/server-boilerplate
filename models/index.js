4e154fc4c022995e9900b62b8a71934bf9d598e8
Nithin Valiyaveedu <nithinvaliya@gmail.com>
2023-03-21 23:45:50 +0530

    added common associations

--- models/index.js
+++ models/index.js
@@ -31,5 +31,91 @@ module.exports = (sequelize) => {
       createdAt: false,
     }
   );
+
+  const ContactInfo = sequelize.define(
+    'ContactInfo',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      phone: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: true,
+    }
+  );
+
+  const Tweet = sequelize.define(
+    'Tweet',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+
+  // one-to-one => hasOne, belongsTo
+
+  User.hasOne(ContactInfo, {
+    foreignKey: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+  });
+  ContactInfo.belongsTo(User);
+
+  //one-to-many => hasMany, belongsTO
+
+  User.hasMany(Tweet, {
+    foreignKey: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+  });
+
+  Tweet.belongsTo(User);
+
+  //many-to-many => belongToMany
+
+  // Example
+
+  // Actor.belongsToMany(Movie, {
+  //   through: 'Actors_Movies',
+  // });
+  // Movie.belongsToMany(Actor, {
+  //   thorugh: "Actor_Movies"
+  // });
+
+  User.belongsToMany(User, {
+    as: 'User',
+    foreignKey: 'UserId',
+    through: 'Follow',
+  });
+
+  User.belongsToMany(User, {
+    as: 'Followed',
+    foreignKey: 'FollowedId',
+    through: 'Follow',
+  });
+
   sequelize.sync({ alter: true });
 };
